739359fa4cbff391429c11247786f20502ade33c
jhsu26 <hsujessica21@gmail.com>
2017-02-09 16:18:30 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equix="X-UA-Compatible" content="IE=edge">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<link rel="stylesheet" href="main.css">
+	
+	<title>Angel Hsu</title>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+	<div class="container-fluid">
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="navbar-header">
+       			 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#angelNav">
+          			<span class="icon-bar"></span>
+          			<span class="icon-bar"></span>
+          			<span class="icon-bar"></span>                        
+      			</button>
+    		</div>
+    		<div>
+      			<div class="collapse navbar-collapse" id="angelNav">
+       			 	<ul class="nav navbar-nav">
+         			</ul>
+         			<ul class="nav navbar-nav navbar-right">
+         				<li><a id="" href="#bg1">About</a></li>
+         				<li><a id="" href="#bg2">Work Experience</a></li>         				
+         				<li><a id="" href="#bg3">Research & Publications</a></li>
+         				<li><a id="" href="#bg4">Contact</a></li>
+         			</ul>
+      			</div>
+    		</div>
+    	</nav>
+		<div class="row" id="bg1">
+			<div class="bg" id="about">
+				<span id="name">ANGEL HSU.</span>
+				<div id="summary-about">
+					Medical student at University of Virginia. Bachelor of Science in Biology and 
+					Bachelor of Art in Spanish from Emory University. Researcher. Medical Interpreter. 
+					Advocate of sexual violence survivors. 
+				</div>
+			</div>
+		</div>
+		<div class="row" id="bg2">
+			<div class="section" id="s1">Work Experiences</div>
+			<div class="bg" id="work">
+				<div id="summary-work">
+					Undergraduate Researcher, Centers of Disease Control<br>
+						<p>Atlanta, GA (Aug '14 - Apr '16)<br>Conducted protein assays (Western Blot, Bradford). Ran experiments for <i>Toxocara canis</i>
+						. Developed assays on ELISA and Luminex for fascioliasis, <i>Strongyloides stercoralis</i>, <i>E. granulosus</i>. Became a member of the American Society 
+						of Tropical Medicine and Hygiene (ASTMH). <br></p>
+					EPASS Peer Tutor, Emory University<br>
+						<p>Atlanta, GA (Sep '14 - Apr '16)<br>Taught organic chemistry, physics, and spanish. Helped students review class notes and past exams, 
+						work through problems, prepare for upcoming exams, build a greater understanding of difficult concepts. 
+						Mentored students to become independent learners by helping them develop more efficient studying skills. 
+						Became member of Association for the Tutoring Profession as of September 2015.
+						<br></p>
+					Peer Facilitator, Emory Sexual Assault Peer Advocate (SAPA)<br>
+						<p>Atlanta, GA (Feb '14 - May '16)<br>rained on how to provide support for sexual assault survivors at Emory. 
+						 Attended weekly Continued Education Meetings on sexual assault prevention and education. 
+						 Led SAPA 101 Trainings in order to teach the student body on how to support survivors. 
+						 Interviewed new SAPA advocates. Attended RespectCon 2016, a conference on sexual violence through a social justice lens.
+						 <br></p>
+					Medical Interpreter, Emory Volunteer Medical Interpreting Services<br>
+						<p>Atlanta, GA (Oct '13 - May '16)<br>Passed a 6-week course on the medical terminology, code of ethics, cultural barriers, patient advocacy, 
+						and techniques of medical interpretation offered by Culture Connect. Volunteered as a medical interpreter at the Good Samaritan 
+						Clinic biweekly. Interpreted at the Physician Assistant (PA) week service trip to help migrant workers in rural Georgia. 
+						Served as co-director of finance and publicity for one year. 
+						<br></p>
+					Hospital Volunteer, Winsihp Cancer Institute & E-da Hospital<br>
+						<p>Atlanta, GA & Kaohsiung, Taiwan (2012 - Oct '15)<br>Winship: Assisted nurses in the blood infusion center. Managed art cart, and helped patients with arts and crafts.
+						 Distributed snacks throughout Winship, emphasizing interaction with patients. Organized and played in violin performances.<br>
+						 E-da: patients and set up health examinations. Assisted nurses in the Emergency Room, Triage Station, and Pediatrics Clinic. 
+						 Measured blood pressure, body temperature, weight, height, waist/hip circumference for patients. Organized and played in various violin performances. 
+						<br></p>
+					Undergraduate Researcher, Emory Cardiothoracic Research Laboratory<br>
+						<p>Atlanta, GA (2013 - May '14)<br>Designed and carried out experiments for effect of KCl on stress-relaxation of mitral valve (MV) leaflet tissue. 
+						Cultured porcine fibroblasts and established protocol for attaching cells to membrane using fibronectin. 
+						Practiced explanting heart valves and suturing opened hearts. Analyzed data using the Radnoti Organ Bath System and Labscribe 
+						and complied work into a formal report summarizing results. Presented results at the Emory Bio499 Research Symposium Spring 2014.
+						<br></p>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="bg3">
+			<div class="section" id="s2">Publications & Honors/Awards</div>
+			<div class="bg" id="publication">
+				<div id="summary-publication">
+					<u>Publications:</u><br>
+					<p>Shin, S. H., Hsu, A., Chastain, H. M., Cruz, L. A., Elder, E. S., Sapp, S. G., . . . Handali, S. (2016). 
+					Development of Two FhSAP2 Recombinant-Based Assays for Immunodiagnosis of Human Chronic Fascioliasis. 
+					American Journal of Tropical Medicine and Hygiene, 95(4), 852-855. doi:10.4269/ajtmh.16-0253<p><br>
+					<u>Honors/Awards</u><br><p>
+					Phi Beta Kappa<br>
+					Alexander Means Scholar<br>
+					Emory College Language Center Award for Excellence in Spanish<br>
+					Phi Sigma (Honor Society for Biological Sciences)<br>
+					Phi Sigma Iota Foreign Language Honor Society<br>
+					Alpha Epsilon Delta (Pre-med) Honor Society<br>
+					EARCOS Global Citizenship Award & Community Service Grant<br></p>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="bg4">
+			<div class="section" id="s3">Contact</div>
+			<div class="bg" id="contact">
+				<div id="contact-form">
+					<form class="form-horizontal" name="myForm" id="myForm" method="get" action="">
+						<div class="form-group">
+							<label for="theirName">Name:</label>
+							<input id="theirName" type="text" class="form-control" name="theirName" placeholder="Your Name" >
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject:</label>
+							<input id="subject" type="text" class="form-control" name="subject" placeholder="Subject">
+						</div>
+						<div class="form-group">
+							<label for="email">Name:</label>
+							<input id="email" type="email" class="form-control" name="email" placeholder="Your Name">
+						</div>
+						<div class="form-group">
+  							<label for="message">Message:</label>
+  							<textarea class="form-control" rows="20" id="message" name="message"></textarea>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
